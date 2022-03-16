8a2fc2e00663b500db4e70e11bb12ee929b180bc
Peter Sirany <peterCFII@hotmail.com>
2022-03-16 14:39:29 -0600

    Initial Commit

---
+++ UILayoutProject/UILayoutProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UILayoutProjectApp.swift
+//  UILayoutProject
+//
+//  Created by Peter Sirany on 3/16/22.
+//
+
+import SwiftUI
+
+@main
+struct UILayoutProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
